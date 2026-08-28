bc6ea02647c0b69d88d1aa002ac8b9b963bca38d
mirnapenaforte <mirnapenaforte.dev@gmail.com>
2026-08-28 15:19:07 -0300

    Entregavel semana 02

---
+++ Java02/src/Carro.java
@@ -0,0 +1,12 @@
+public class Carro {
+    String marca;
+    int ano;
+    boolean venda;
+
+    public Carro(String marca, int ano, boolean venda){
+
+        this.marca = marca;
+        this.ano = ano;
+        this.venda = venda;
+    }
+}
---
+++ Java02/src/Pessoa.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+    String nome;
+    int idade;
+    double altura;
+    boolean empregado;
+
+    public Pessoa(String nome, int idade, double altura, boolean empregado){
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.empregado = empregado;
+    }
+
+}
